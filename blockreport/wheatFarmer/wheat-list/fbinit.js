31da6f6d7aad96c2902d36aef211641abdb8b868
daily-report <98112289+daily-report@users.noreply.github.com>
2022-04-11 18:12:11 +0600

    Add files via upload

---
+++ blockreport/wheatFarmer/wheat-list/fbinit.js
@@ -0,0 +1,15 @@
+ // Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyDYc9XFwVqKzbVFl6qp-65_2btGGA5c2Bw",
+		authDomain: "blockreport-fc82c.firebaseapp.com",
+		databaseURL: "https://blockreport-fc82c-default-rtdb.firebaseio.com",
+		projectId: "blockreport-fc82c",
+		storageBucket: "blockreport-fc82c.appspot.com",
+		messagingSenderId: "745182379010",
+		appId: "1:745182379010:web:75230cc51c69417bf07b1b",
+		measurementId: "G-TZT2J6LBGS"
+	};
+		firebase.initializeApp(config);
+		var database = firebase.database();
+		
+		
